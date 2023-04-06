5bc37f062242dc326a347f6bfd4a3cf198d7a642
LuongXuanThang20110724 <20110724@student.hcmute.edu.vn>
2023-04-06 07:42:51 +0700

    update fe_v2 ver2

---
+++ fe_v2/src/App.js
@@ -0,0 +1,14 @@
+import ServiceLevel from "./pages/ServiceLevel"
+import Inbound from "./pages/Inbound"
+import Agents from "./pages/Agents"
+import Numbers from "./pages/Numbers";
+
+function App() {
+  return (
+    <div className="App">
+      <ServiceLevel />
+    </div>
+  );
+}
+
+export default App;
